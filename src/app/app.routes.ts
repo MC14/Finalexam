aa52e9405f1136a2db2ee9335c467101895a91f2
Morgan Cowans <cowansm@sheridancollege.ca>
2024-04-16 10:46:00 -0400

    Made List & Table work, to be truthyerror stops code from working don't know how to fix it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import {CarListComponent} from "./car-list/car-list.component";
+import {CarDetailsComponentComponent} from "./car-list/car-details-component/car-details-component.component";
+
 
 export const routes: Routes = [
-  {path:'car-list', component:CarListComponent}
+  {path:'car-list', component:CarListComponent},
+  {path:'car/:carID',component:CarDetailsComponentComponent}
 ];
