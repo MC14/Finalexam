9e39ba90ac1e9112ea6af225656715115c9f0dee
Morgan Cowans <cowansm@sheridancollege.ca>
2024-04-16 10:08:01 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CarListService} from "./car-list-service";
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {CarListComponent} from "./car-list/car-list.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'car-list', component:CarListComponent}
+];
